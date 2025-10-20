df4bee0b9b27eb3050eb09c05453ab4feed330b8
BanhTuanKiet <kiett5153@gmail.com>
2025-10-20 14:10:51 +0700

    15th commit

--- src/lib/projectContent.ts
+++ src/lib/projectContent.ts
@@ -80,10 +80,7 @@ export const projectContent = {
                     "Tích hợp Cloudinary để lưu trữ hình ảnh và Firebase cho thông báo"
                 ],
                 images: [
-                    "/images/e-commerce1.png",
                     "/images/e-commerce2.png",
-                    "/images/e-commerce1.png",
-                    "/images/e-commerce1.png",
                 ],
                 teamSize: 1,
                 duration: "Tháng 7 - 9/2025",
